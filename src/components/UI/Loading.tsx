4355dc9eb355ad021d23d015f7db451d93cd490d
MohammadJewelRana <js.rana0326@gmail.com>
2024-10-07 10:57:07 +0600

    basic setup

---
+++ src/components/UI/Loading.tsx
@@ -0,0 +1,15 @@
+// import { Spinner } from "@nextui-org/spinner";
+
+import { Spinner } from "@nextui-org/spinner";
+
+ 
+
+const Loading = () => {
+  return (
+    <div className="bg-black/10  h-screen fixed inset-0 z-[999] backdrop-blur-md flex justify-center items-center">
+      <Spinner size="lg" />
+    </div>
+  );
+};
+
+export default Loading;
